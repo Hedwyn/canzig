bc3d48af369a332d80fd2b1e1756bfae6d4631a8
Hedwyn <bpestourie@gmail.com>
2024-11-18 14:24:26 +0100

    add example standalone code for xml usage in main

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const can = @import("socketcan.zig");
+const xml = @import("xml.zig");
 const debugPrint = std.log.debug;
 
 const default_can_if = "vcan0";
@@ -10,6 +11,7 @@ pub fn main() !void {
     _ = args_it.next();
     const user_can_if: ?[]const u8 = args_it.next();
     const can_if = user_can_if orelse default_can_if;
+    const db_path = args_it.next() orelse unreachable;
 
     if (user_can_if == null) {
         std.debug.print("Defaulting to {s}\n", .{can_if});
@@ -27,5 +29,27 @@ pub fn main() !void {
         .data = data,
     };
     _ = try can.canSend(fd, &test_frame);
+
+    const file = try std.fs.cwd().openFile(db_path, .{});
+    const reader = file.reader();
+    const allocator = std.heap.page_allocator;
+    const buffer = try reader.readAllAlloc(allocator, 10_000_000);
+    std.debug.print("{s}", .{buffer});
+    const document = try xml.parse(allocator, buffer);
+    defer document.deinit();
+
+    const root_node = document.root;
+    var it = root_node.iterator();
+
+    while (it.next()) |child| {
+        switch (child.*) {
+            .char_data => |d| std.debug.print("Char data= {s}\n", .{d}),
+            .element => |el| std.debug.print("Element, tag={s}\n", .{el.tag}),
+            .comment => |c| std.debug.print("Comment= {s}\n", .{c}),
+        }
+        std.debug.print("Next element = {}\n", .{child});
+    }
+    std.debug.print("{}", .{document});
+    allocator.free(buffer);
     debugPrint("Received {}", .{can.canRecv(fd)});
 }
