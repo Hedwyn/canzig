f892723a96fa810f0c082b4b0a08b86194f9c102
Hedwyn <bpestourie@gmail.com>
2025-02-21 14:03:15 +0100

    add kcd parsing demo code to main

--- src/main.zig
+++ src/main.zig
@@ -1,22 +1,31 @@
 const std = @import("std");
 const can = @import("socketcan.zig");
 const xml = @import("xml.zig");
+const kcd = @import("kcd.zig");
 const easycli = @import("parser");
 const debugPrint = std.log.debug;
 
-const default_can_if = "vcan0";
+const default_can_if: []const u8 = "vcan0";
 
 const Options = struct {
-    interface: []const u8,
-    db_path: []const u8,
+    interface: []const u8 = default_can_if,
+    db_path: ?[]const u8 = null,
+};
+
+const options_doc = [_]easycli.OptionInfo{
+    .{ .name = "interface", .help = "Name of the CAN channel to connect to" },
+    .{ .name = "db_path", .help = "Path to a KCD database to parse" },
 };
 
 pub fn main() !void {
-    const ParserT = easycli.CliParser(Options, struct {});
-    const params = if (try ParserT.runStandalone(.{})) |p| p else return;
+    const ParserT = easycli.CliParser(.{
+        .opts = Options,
+        .opts_info = &options_doc,
+    });
+    const params = if (try ParserT.runStandalone()) |p| p else return;
     const can_if = params.options.interface;
-    const db_path = params.options.db_path;
-    std.debug.print("Can if is {s}\n", .{can_if});
+
+    std.debug.print("Can if is {s}\n", .{params.options.interface});
     const fd = try can.openSocketCan(can_if);
     defer can.closeSocketCan(fd);
     const data = [_]u8{ 1, 2, 3, 4, 5, 6, 7, 8 };
@@ -27,27 +36,18 @@ pub fn main() !void {
         .data = data,
     };
     _ = try can.canSend(fd, &test_frame);
+    std.debug.print("path!{any} \n", .{params.options.db_path});
 
-    const file = try std.fs.cwd().openFile(db_path, .{});
-    const reader = file.reader();
-    const allocator = std.heap.page_allocator;
-    const buffer = try reader.readAllAlloc(allocator, 10_000_000);
-    std.debug.print("{s}", .{buffer});
-    const document = try xml.parse(allocator, buffer);
-    defer document.deinit();
+    // --- KCD demo ---
+    if (params.options.db_path) |db_path| {
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        defer arena.deinit();
+        const allocator = arena.allocator();
+        const file = try std.fs.cwd().openFile(db_path, .{});
 
-    const root_node = document.root;
-    var it = root_node.iterator();
-
-    while (it.next()) |child| {
-        switch (child.*) {
-            .char_data => |d| std.debug.print("Char data= {s}\n", .{d}),
-            .element => |el| std.debug.print("Element, tag={s}\n", .{el.tag}),
-            .comment => |c| std.debug.print("Comment= {s}\n", .{c}),
-        }
-        std.debug.print("Next element = {}\n", .{child});
+        const reader = file.reader();
+        const buffer = try reader.readAllAlloc(allocator, 10_000_000);
+        const database = try kcd.parseKcd(allocator, buffer);
+        std.log.debug("Database = {}\n", .{database});
     }
-    std.debug.print("{}", .{document});
-    allocator.free(buffer);
-    debugPrint("Received {}", .{can.canRecv(fd)});
 }
